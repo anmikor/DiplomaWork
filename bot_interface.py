91b9995f90c4eb141e558cf5640a772d2091ee78
anmikor <127435884+anmikor@users.noreply.github.com>
2023-05-10 17:06:10 +0300

    Update bot_interface.py
    Более подробно описаны импорты, расширен класс NewData,
    упрощен вывод в чате параметров поиска, добавлены функции обработки данных поиска и отправки

--- bot_interface.py
+++ bot_interface.py
@@ -1,7 +1,10 @@
+import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
-from bd import *
-from my_cod import *
+from bd import extract_from_db, add_to_table, create_tables
+import sqlalchemy as sq
+from sqlalchemy.orm import sessionmaker
+from my_cod import search_params, VkTools
 from config import user_token, group_token
 
 tools = VkTools(user_token)
@@ -18,26 +21,36 @@ session.close()
 class NewData:
     def __init__(self):
         self.town = "Калуга"
-        self.city_id = 66
-        self.sex = 2
+        self.city_id = 62
+        self.sex = 1
         self.age_from = 25
         self.age_to = 35
 
-    def __getitem__(self, item):
+    @property
+    def getlist(self):
         return [self.town, self.city_id, self.sex, self.age_from, self.age_to]
 
+    @getlist.setter
+    def getlist(self, data):
+        self.town = data[0]
+        self.city_id = data[1]
+        self.sex = data[2]
+        self.age_from = data[3]
+        self.age_to = data[4]
+
     def __str__(self):
-        return (f'Город: {self.town}| '
-                f'Id города: {self.city_id}| '
-                f'Пол: {self.sex}| '
-                f'Возраст от: {self.age_from}| '
-                f'Возраст до: {self.age_to}')
+        return (f'Параметры поиска:\n'
+                f'Город:        {self.town}\n'
+                f'Id города:    {self.city_id}\n'
+                f'Пол:          {self.sex}\n'
+                f'Возраст от:   {self.age_from} до: {self.age_to}.')
 
 
 class BotInterface:
 
     def __init__(self, token):
         self.bot = vk_api.VkApi(token=token)
+        self.profiles_list = []
 
     def message_send(self, user_id, message=None, attachment=None):
         """
@@ -67,6 +80,9 @@ class BotInterface:
                 self_user_data.sex = start_data[2]
 
                 if msg_to_me == 'привет':
+                    
+                    """  Анализ данных аккаунта на недостающие данные  """
+                    
                     self.message_send(peer_id, f'Привет, {client["first_name"]}')
                     if self_user_data.town == 0:
                         self.message_send(peer_id, f'{client["first_name"]}, для поиска недостаточно данных.\n'
@@ -78,13 +94,27 @@ class BotInterface:
                                                    f'Введите, пожалуйста интервал возраста для поиска\n'
                                                    f'В формате: 22-33')
                     else:
-                        self.message_send(peer_id, f'Теперь чтобы искать по вашим '
-                                                   f'анкетным данным, введите: поиск \n'
-                                                   f'А если нужно уточнить город или возраст для поиска,\n'
-                                                   f'введите вначале город в формате: г Москва\n'
-                                                   f'Потом возраст в формате 28-32\n'
-                                                   f'а затем команду: далее')
+                        self.message_send(peer_id, f'Чтобы искать по анкетным данным,\n '
+                                                   f'которые вы указали на странице ВК, \n '
+                                                   f'введите: поиск \n')
+                elif msg_to_me == 'поиск':
+                    self_user_data.getlist = start_data
+
+                    if self_user_data.town == 0 or self_user_data.age_from == 0:
+
+                        self.message_send(peer_id, f'Для поиска недостаточно данных.\n'
+                                                   f'{self_user_data} ')
+
+                    else:
+                        self.finder(self_user_data.getlist)
+                        self.profiles_manager(peer_id)
+                        self.message_send(peer_id, f'Если вам нужны другие анкеты по тем же параметрам, '
+                                                   f'снова введите: поиск \n'
+                                                   f'Чтобы остановить бота, напишите: стоп\n'
+                                                   f'Чтобы изменить параметры поиска, напишите: далее')
+                        
                     """  Ввод города  """
+                    
                 elif (msg_to_me.split(' '))[0] == 'г':
                     town = msg_to_me.split(' ')[1].capitalize()
                     new_city_id = tools.find_city_id(f'{town}')
@@ -92,8 +122,7 @@ class BotInterface:
                     self_user_data.city_id = new_city_id
                     print(self_user_data)
 
-                    self.message_send(peer_id, f'Теперь требуется ввести интервал возраста\n'
-                                               f'в формате: 25-30')
+                    self.message_send(peer_id, f'Теперь введите  команду: далее')
 
                     """  Ввод интервала возраста   """
 
@@ -106,68 +135,73 @@ class BotInterface:
 
                     self.message_send(peer_id, f'Теперь введите  команду: далее')
 
-                elif msg_to_me == 'поиск':
-                    self_user_data.town = start_data[0]
-                    self_user_data.city_id = start_data[1]
-                    self_user_data.age_from = start_data[3]
-                    self_user_data.age_to = start_data[4]
-
+                elif msg_to_me == 'далее':
                     if self_user_data.town == 0 or self_user_data.age_from == 0:
-
                         self.message_send(peer_id, f'Для поиска недостаточно данных.\n'
-                                                   f'--Город:                      {self_user_data.town},\n'
-                                                   f'--id города                   {self_user_data.city_id}\n'
-                                                   f'--Пол (1-жен., 2-муж.):       {self_user_data.sex}\n'
-                                                   f'--Минимальный возраст:        {self_user_data.age_from}\n'
-                                                   f'--Максимальный возраст:       {self_user_data.age_to}.')
+                                                   f'{self_user_data} ')
 
                     else:
-                        self.sender(peer_id, start_data)
-                    self.message_send(peer_id, f'Если этих результатов недостаточно, '
-                                               f'снова введите: поиск \n'
-                                               f'Чтобы остановить поиск, напишите: стоп\n'
-                                               f'1. Надо изменить город поиска: г Калуга\n'
-                                               f'2. Ввести интервал возраста, например: 28-32\n'
-                                               f'3. После этого введите команду: далее')
+                        self.message_send(peer_id,   self_user_data)
 
-                elif msg_to_me == 'далее':
+                        self.finder(self_user_data.getlist)
+                        self.profiles_manager(peer_id)
 
-                    self.message_send(peer_id,   f'--Город:                      {self_user_data.town},\n'
-                                                 f'--id города                   {self_user_data.city_id}\n'
-                                                 f'--Пол (1-жен., 2-муж.):       {self_user_data.sex}\n'
-                                                 f'--Минимальный возраст:        {self_user_data.age_from}\n'
-                                                 f'--Максимальный возраст:       {self_user_data.age_to}.')
-
-                    self.sender(peer_id, self_user_data)
-
-                    self.message_send(peer_id, f'Доступные команды: поиск, далее, стоп\n'
-                                               f'Изменение данных поиска в комментарии '
-                                               f'к команде: привет')
+                        self.message_send(peer_id, f'Доступные команды: далее, стоп, привет\n'
+                                                   f'Команда "пуск" ищет только по анкетным данным в ВК,\n'
+                                                   f'если все данные указаны в вашем аккаунте')
 
                 elif msg_to_me == 'стоп':
                     break
                 else:
                     self.message_send(peer_id, 'Напишите привет, чтобы получить информацию')
 
-    def sender(self, peer_id, data):
-        print(data)
-        profiles_list = tools.find_users(data)
-        for profile in profiles_list:
-            info_db = extract_from_db(session, profile['id'])
-            if info_db == 1:
-                continue
-            else:
-                add_to_table(session, profile_id=peer_id, account_id=profile['id'])
+        def finder(self, data: list, offset=None):
+        """ Выполняет поиск и
+            Формирует список профилей, не добавленных в базу данных
+        """
+        if len(self.profiles_list) == 0:
+            profiles_list = tools.find_users(data, offset=offset)
+            for profile in profiles_list:
+                info_db = extract_from_db(session, profile['id'])
+                if info_db == 1:
+                    continue
+                elif info_db == 0:
+                    self.profiles_list.append(profile)
+        else:
+            pass
 
-            photos = tools.find_photos(profile['id'])
-            self.message_send(peer_id, f'Ссылка на аккаунт: {profile["name"]}'
-                                       f' https://vk.com/id{profile["id"]} '
-                                       f'город {data[0][0]}')
+    def profiles_manager(self, peer_id):
+        """ Если сформированный список профилей пустой,
+            то вызывает
+            поиск новых профилей, увеличивая значение offset,
+            а если непустой,
+            то вызывает отправитель фото sender() и добавляет запись в базу данных,
+            обрезая список.
+        """
+        offset = 0
+        spl = self.profiles_list
+        if len(spl) == 0:
+            while len(spl) == 0:
+                offset += 30
+                print(offset)
+                self.finder(self_user_data.getlist, offset=offset)
 
-            for num, photo in enumerate(photos):
-                attachment = f'photo{photo["owner_id"]}_{photo["id"]}'
-                self.message_send(peer_id, f'Фото номер {num + 1} ', attachment)
-            break
+        elif len(spl) > 0:
+            self.sender(peer_id, spl[-1])
+            add_to_table(session, profile_id=peer_id, account_id=spl[-1]['id'])
+            self.profiles_list.pop()
+
+    def sender(self, peer_id, profile):
+        """  Ицет и отправляет фотографии выбранного профиля
+        """
+        photos = tools.find_photos(profile['id'])
+        self.message_send(peer_id, f'Ссылка на аккаунт: {profile["name"]}'
+                                   f' https://vk.com/id{profile["id"]} '
+                                   f'Город {self_user_data.town}')
+
+        for num, photo in enumerate(photos):
+            attachment = f'photo{photo["owner_id"]}_{photo["id"]}'
+            self.message_send(peer_id, f'Фото номер {num + 1} ', attachment)
 
 
 if __name__ == '__main__':
